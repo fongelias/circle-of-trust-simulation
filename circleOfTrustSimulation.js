ce8767892f57926080f150abcca89f138547ffec
Elias Fong <fongelias@gmail.com>
2019-06-13 15:01:19 -0400

    added start of file

---
+++ circleOfTrustSimulation.js
@@ -0,0 +1,21 @@
+// What do we want to simulate?
+// How many code owners do we need per section of code?
+// Take into account:
+// Imperfect reviewers- they make mistakes x% of the time
+// Imperfect developers- they make mistakes x% of the time
+// Time for review- how long does it take to review T tasks with R reviewers?
+// Time to review- how long does it take for a review to begin?
+
+class Developer() {
+
+}
+
+class Reviewer() {
+
+}
+
+class Task() {
+
+}
+
+class 
