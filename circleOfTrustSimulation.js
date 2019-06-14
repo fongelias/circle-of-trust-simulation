1e56b7604f57e3bd6ad294d63b02015a8f547556
Elias Fong <fongelias@gmail.com>
2019-06-14 15:13:27 -0400

    added states to task const

--- circleOfTrustSimulation.js
+++ circleOfTrustSimulation.js
@@ -6,16 +6,47 @@
 // Time for review- how long does it take to review T tasks with R reviewers?
 // Time to review- how long does it take for a review to begin?
 
-class Developer() {
+// Events
+const EVENTS = {};
 
+const EventEmitter = require('events');
+const SCM = new EventEmitter(); // source control as an event emitter
+
+// Roles
+class Developer {
+	constructor(scm) {
+		this.scm = scm;
+	}
 }
 
-class Reviewer() {
-
+class Reviewer {
+	constructor(scm) {
+		this.scm = scm;
+	}
 }
 
-class Task() {
-
+// Task
+const TASK = {
+	STATE: {
+		TO_DO: 'todo',
+		IN_PROGRESS: 'in progress',
+		READY_FOR_REVIEW: 'ready for review',
+		IN_REVIEW: 'in review',
+		COMPLETE: 'complete'
+	}
 }
 
-class 
+class Task {
+	constructor() {
+		this.state = TASK.STATE.TO_DO;
+	}
+
+	start() {
+		if (this.state != TASK.STATE.TO_DO) {
+			throw new Error('cannot transition task to in progress from ' + this.state);
+		}
+		
+		this.state = TASK.STATE.IN_PROGRESS;
+	}
+
+}
